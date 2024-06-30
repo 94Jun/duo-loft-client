8f03a20edf3751e5357bd2927a4016453fd4f41b
seoa <naeil_do@naver.com>
2024-06-30 20:28:48 +0900

    change project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import templogo from "./temp_logo.png";
+import templogo from "./panda_logo.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="nav"> 홈 | 마이페이지 | 앨범 | 도장깨기</div>
-      <img src={templogo} alt="temp" />
-      <h1>Duo-Loft</h1>
-      <h3> 커플이 되어 탈출하세요!</h3>
+      <section className="logo">
+        <img src={templogo} alt="temp" width={"500px"} />
+      </section>
     </div>
   );
 }
---
+++ src/menu/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const componentName = () => {
+  return <>메인</>;
+};
+
+export default componentName;
